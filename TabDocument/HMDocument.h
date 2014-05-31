113ba9a4adbc619c3fae2a6222b4ce88b80f6054
Scott Horn <scott.horn@wotifgroup.com>
2014-06-01 07:42:49 +1000

    Initial Commit

---
+++ TabDocument/HMDocument.h
@@ -0,0 +1,13 @@
+//
+//  HMDocument.h
+//  TabDocument
+//
+//  Created by Scott Horn on 1/06/2014.
+//  Copyright (c) 2014 Scott Horn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HMDocument : NSDocument
+
+@end
---
+++ TabDocument/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TabDocument
+//
+//  Created by Scott Horn on 1/06/2014.
+//  Copyright (c) 2014 Scott Horn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
